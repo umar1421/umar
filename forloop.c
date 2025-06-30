e8f6bf501aa9a4d5af185a6c188b0704bda98a18
umar1421 <www.ummiumar1421@gmail.com>
2025-06-30 11:53:05 +0530

    Create forloop.c

---
+++ forloop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    printf("enter the numbers ");
+    scanf("%d",&j);
+
+    for (i=1 ; i <=j ; ++i)
+{
+ k+=i;
+}   
+printf("%d",k);
+    return 0;
+}
+// Find sum up n numbers
