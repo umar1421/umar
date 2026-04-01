e998e2e4c86c43a86e807c241c9f9949ee75cabc
umar1421 <www.ummiumar1421@gmail.com>
2026-04-02 00:03:27 +0530

    Create String.C

---
+++ String.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char str1[] = "Hello";
+    char str2[] = "World";
+
+    printf("String 1: %s\n", str1);
+    printf("String 2: %s\n", str2);
+
+    return 0;
+}
